3cf3acb0252a3d211958dd2401f9b3b1a37b9fbb
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2021-02-23 13:05:05 +0300

    Create MK 3 sub

---
+++ Diamond_MK_Maksim/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Diamond_MK_Maksim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
