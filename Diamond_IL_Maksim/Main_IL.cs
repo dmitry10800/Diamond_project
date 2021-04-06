09f2e2768f66ac30e06f79c9a68206fd0428f7db
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2021-04-06 08:46:22 +0300

    Added 19 sub in IL

---
+++ Diamond_IL_Maksim/Main_IL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Diamond_IL_Maksim
+{
+    class Main_IL
+    {
+
+        private readonly static string Path = @"C:\Work\IL\IL_20210325_03";
+        private readonly static string SubCode = "19";
+        private readonly static bool SendToProduction = false;    // если false то отправляет на стейджинг - если true то отправляет на продакшен
+
+        static void Main(string[] args)
+        {
+            Methods methods = new();
+
+            List<Diamond.Core.Models.LegalStatusEvent> convertedPatents = SubCode switch
+            {
+                "19" => methods.Start(Path, SubCode),
+                _ => null
+            };
+
+            Console.WriteLine();
+
+            if (convertedPatents != null) methods.SendToDiamond(convertedPatents, SendToProduction);
+            else Console.WriteLine("Wrong sub code");
+        }
+    }
+}
---
+++ Diamond_IL_Maksim/Methods.cs
@@ -0,0 +1,149 @@
+﻿using Integration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Diamond_IL_Maksim
+{
+    class Methods
+    {
+        private string CurrentFileName;
+        private int Id = 1;
+
+        internal List<Diamond.Core.Models.LegalStatusEvent> Start (string path, string subCode)
+        {
+            List<Diamond.Core.Models.LegalStatusEvent> statusEvents = new();
+
+            DirectoryInfo directory = new(path);
+
+            List<string> files = new();
+
+            foreach (FileInfo file in directory.GetFiles("*.tetml", SearchOption.AllDirectories))
+            {
+                files.Add(file.FullName);
+            }
+
+            XElement tet;
+
+            List<XElement> xElements = new();
+
+            foreach (string tetml in files)
+            {
+                CurrentFileName = tetml;
+
+                tet = XElement.Load(tetml);
+
+                if (subCode == "19")
+                {
+                    using StreamReader textFromFile = new(tetml.Replace(".tetml",".txt"));
+
+                    List<string> notes = Regex.Split(textFromFile.ReadToEnd().Replace("\r", "").Replace("\t", "").Trim(), @"\n").Where(val => !string.IsNullOrEmpty(val)).Where(val => !val.StartsWith(" ")).ToList();
+
+                    foreach (string note in notes)
+                    {
+                        foreach (Diamond.Core.Models.LegalStatusEvent item in MakePatent(note, subCode, "BZ"))
+                        {
+                            statusEvents.Add(item);
+                        }
+                        
+                    }
+                }
+            }
+            return statusEvents;
+        }
+
+        internal List <Diamond.Core.Models.LegalStatusEvent> MakePatent (string note, string subCode, string sectionCode)
+        {
+            Diamond.Core.Models.LegalStatusEvent legal = new()
+            {
+                SectionCode = sectionCode,
+                SubCode = subCode,
+                CountryCode = "IL",
+                Id = Id++,
+                GazetteName = Path.GetFileName(CurrentFileName.Replace(".tetml", ".pdf"))
+            };
+
+            List<Diamond.Core.Models.LegalStatusEvent> legals = new();
+
+            Biblio biblio = new();
+
+            Match match = Regex.Match(note.Trim(), @"(?<gr1>\D+)\s?-\s?(?<gr2>.+)");
+
+            if (match.Success)
+            {
+                List<string> numbers = Regex.Split(match.Groups["gr2"].Value.Trim(), ",").Where(val => !string.IsNullOrEmpty(val)).ToList();
+
+                foreach (string number in numbers)
+                {
+                    biblio.Application.Number = number.Trim();
+
+                    Match match1 = Regex.Match(match.Groups["gr1"].Value.Trim(), @"[A-Z]");
+                    if (match1.Success)
+                    {
+                        biblio.Applicants = new()
+                        {
+                            new PartyMember
+                            {
+                                Name = match.Groups["gr1"].Value.Trim(),
+                                Language = "EN"
+                            }
+                        };
+                    }
+                    else
+                    {
+                        biblio.Applicants = new()
+                        {
+                            new PartyMember
+                            {
+                                Name = match.Groups["gr1"].Value.Trim(),
+                                Language = "HE"
+                            }
+                        };
+                    }                   
+                    Match date = Regex.Match(Path.GetFileName(CurrentFileName.Replace(".txt", "")), @"[0-9]{8}");
+                    if (date.Success)
+                    {
+                        biblio.DOfPublication = new()
+                        {
+                            date_45 = date.Value.Insert(4, "-").Insert(7, "-").Trim()
+                        };
+                    }
+                    legal.Biblio = biblio;
+                    legals.Add(legal);
+                }
+            }
+
+            return legals;
+        }
+        internal void SendToDiamond(List<Diamond.Core.Models.LegalStatusEvent> events, bool SendToProduction)
+        {
+            foreach (var rec in events)
+            {
+                string tmpValue = JsonConvert.SerializeObject(rec);
+                string url = null;
+                if (SendToProduction == true)
+                {
+                    url = @"https://diamond.lighthouseip.online/external-api/import/legal-event";  // продакшен
+                }
+                else
+                {
+                    url = @"https://staging.diamond.lighthouseip.online/external-api/import/legal-event";     // стейдж
+                }
+                HttpClient httpClient = new();
+                httpClient.BaseAddress = new Uri(url);
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                StringContent content = new(tmpValue.ToString(), Encoding.UTF8, "application/json");
+                HttpResponseMessage result = httpClient.PostAsync("", content).Result;
+                string answer = result.Content.ReadAsStringAsync().Result;
+            }
+        }
+    }
+}
