ef0b86e41590df914af1cbe13b6641de3301d025
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2021-04-19 09:49:39 +0300

    formate DiamondProjectClasses

---
+++ Countries/DiamondClasses/Claim.cs
@@ -0,0 +1,16 @@
+﻿using Integration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiamondProjectClasses
+{
+    public class Claim
+    {
+        public string Language { get; set; }
+        public string Number { get; set; }
+        public string Text { get; set; }
+        public List<Translation> Translations { get; set; }
+        public List<ScreenShot> ScreenShots { get; set; }
+    }
+}
