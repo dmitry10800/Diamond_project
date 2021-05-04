08a1163915ec50475c817ffc22934ed9291e1ef7
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2021-05-04 08:58:37 +0300

    Added 1 sub Code in PA

---
+++ Diamond_PA_Maksim/Main_PA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Diamond_PA_Maksim
+{
+    class Main_PA
+    {
+
+        private static readonly string Path = @"C:\Work\PA\PA_20210422_383";
+        private static readonly string SubCode = "1";
+        private static readonly bool SendToProd = false;   // true - send to Prod ; false - send to Stag
+
+        static void Main(string[] args)
+        {
+            Methods methods = new();
+
+            List<Diamond.Core.Models.LegalStatusEvent> patents = SubCode switch
+            {
+                "1" => methods.Start(Path, SubCode),
+                "2" => methods.Start(Path, SubCode),
+                _ => null
+            };
+
+            Console.WriteLine();
+
+            if (patents != null) methods.SendToDiamond(patents, SendToProd);
+            else Console.WriteLine("Wrong subcode");
+        }
+    }
+}
