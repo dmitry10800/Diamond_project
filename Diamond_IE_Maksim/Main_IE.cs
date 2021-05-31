725fa6a24169821114994c5c699477dfef58a012
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2021-05-31 15:55:11 +0300

    Done 52 sub Code IE

---
+++ Diamond_IE_Maksim/Main_IE.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Diamond_IE_Maksim
+{
+    class Main_IE
+    {
+
+        private static readonly string Path = @"C:\Work\IE\IE_20210428_2436";
+        private static readonly string SubCode = "52";
+        private static readonly bool SendToProd = false;   // true - send to Prod ; false - send to Stag
+
+        static void Main(string[] args)
+        {
+            Methods methods = new();
+
+            List<Diamond.Core.Models.LegalStatusEvent> patents = SubCode switch
+            {
+                "52" => methods.Start(Path, SubCode),
+                _ => null
+            };
+
+            Console.WriteLine();
+
+            if (patents != null) methods.SendToDiamond(patents, SendToProd);
+            else Console.WriteLine("Wrong subcode");
+        }
+    }
+}
