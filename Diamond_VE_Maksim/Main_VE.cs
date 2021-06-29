9656c18f0baf11dc40c612172013e79b22d483ec
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2021-06-29 19:28:02 +0300

    Development is suspended  NG/VE

---
+++ Diamond_VE_Maksim/Main_VE.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Diamond_VE_Maksim
+{
+    class Main_VE
+    {
+        private static readonly string Path = @"C:\Work\VE\VE_20190702_594";
+        private static readonly string SubCode = "24";
+        private static readonly bool SendToProd = false;   // true - send to Prod ; false - send to Stag
+        static void Main(string[] args)
+        {
+            Methods methods = new();
+
+            List<Diamond.Core.Models.LegalStatusEvent> patents = SubCode switch
+            {
+                "24" => methods.Start(Path, SubCode),
+                _ => null
+            };
+
+            Console.WriteLine();
+
+            if (patents != null) methods.SendToDiamond(patents, SendToProd);
+            else Console.WriteLine("Wrong subcode");
+        }
+    }
+}
