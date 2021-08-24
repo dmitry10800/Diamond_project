57a6bc9acd7542d0fc010e4296486a219d8866f5
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2021-08-24 12:43:01 +0300

    Added 12 sub in EA  .  Upgraded 5, 11 subs in EA

--- DIamond_EA_Maksim/Main_EA.cs
+++ DIamond_EA_Maksim/Main_EA.cs
@@ -6,8 +6,8 @@ namespace DIamond_EA_Maksim
     class Main_EA
     {
 
-        private static readonly string Path = @"C:\Work\EA\EA_20210531_05";
-        private static readonly string SubCode = "5";
+        private static readonly string Path = @"C:\Work\EA\EA_20210731_07";
+        private static readonly string SubCode = "12";
         private static readonly bool SendToProd = false;   // true - send to Prod ; false - send to Stag
         static void Main(string[] args)
         {
@@ -19,6 +19,7 @@ namespace DIamond_EA_Maksim
                 "5" => methods.Start(Path, SubCode),
                 "9" => methods.Start(Path, SubCode),
                 "11" => methods.Start(Path, SubCode),
+                "12" => methods.Start(Path, SubCode),
                 "14" => methods.Start(Path, SubCode),
                 "31" => methods.Start(Path,SubCode),
                 _ => null
