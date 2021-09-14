0f60689cd595c63ac2ffa51733c449c82bcd9a18
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2021-09-14 12:22:12 +0300

    Added 8 subCode in UY

---
+++ Diamond_UY_Maksim/Main_UY.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Diamond_UY_Maksim
+{
+    class Main_UY
+    {
+
+        private static readonly string Path = @"C:\Work\UY\UY_20210831_263";
+        private static readonly string SubCode = "8";
+        private static readonly bool SendToProd = false;   // true - send to Prod ; false - send to Stag
+
+        static void Main(string[] args)
+        {
+            Methods methods = new();
+
+            List<Diamond.Core.Models.LegalStatusEvent> patents = SubCode switch
+            {
+                "8" => methods.Start(Path, SubCode),
+                _ => null
+            };
+
+            Console.WriteLine();
+
+            if (patents != null) methods.SendToDiamond(patents, SendToProd);
+            else Console.WriteLine("Wrong subcode");
+        }
+    }
+}
