5be2b8cf14c73f7b046eb1ba0c4640f8cb816435
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2021-09-24 15:16:06 +0300

    Done 4 subCode in BG

--- Diamond_BG_Maksim/Main_BG.cs
+++ Diamond_BG_Maksim/Main_BG.cs
@@ -6,8 +6,8 @@ namespace Diamond_BG_Maksim
     class Main_BG
     {
 
-        private static readonly string Path = @"C:\Work\BG\BG_20210816_08(1)";
-        private static readonly string SubCode = "21";
+        private static readonly string Path = @"C:\Work\BG\BG_20210915_09(1)";
+        private static readonly string SubCode = "4";
         private static readonly bool SendToProd = false; //true - send to prod; false - send to stag
 
         static void Main(string[] args)
@@ -18,6 +18,7 @@ namespace Diamond_BG_Maksim
             {
                 "1" => methods.Start(Path,SubCode),
                 "3" => methods.Start(Path, SubCode),
+                "4" => methods.Start(Path, SubCode),
                 "5" => methods.Start(Path, SubCode),
                 "7" => methods.Start(Path, SubCode),
                 "21" => methods.Start(Path, SubCode),
