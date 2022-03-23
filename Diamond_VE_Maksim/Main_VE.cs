9691302c0f45f9122dec5a7c2a91817a99dab2e2
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2022-03-23 15:15:30 +0300

    Added 12/19 VE subs

--- Diamond_VE_Maksim/Main_VE.cs
+++ Diamond_VE_Maksim/Main_VE.cs
@@ -5,8 +5,8 @@ namespace Diamond_VE_Maksim
 {
     class Main_VE
     {
-        private static readonly string Path = @"C:\Work\VE\VE_20190702_594";
-        private static readonly string SubCode = "24";
+        private static readonly string Path = @"C:\!Work\VE\VE_20220217_614";
+        private static readonly string SubCode = "12";
         private static readonly bool SendToProd = false;   // true - send to Prod ; false - send to Stag
         static void Main(string[] args)
         {
@@ -14,6 +14,8 @@ namespace Diamond_VE_Maksim
 
             List<Diamond.Core.Models.LegalStatusEvent> patents = SubCode switch
             {
+                "12" => methods.Start(Path, SubCode),
+                "19" => methods.Start(Path, SubCode),
                 "24" => methods.Start(Path, SubCode),
                 _ => null
             };
