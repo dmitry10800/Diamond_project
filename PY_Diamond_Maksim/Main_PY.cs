deb18dfd1c4750645a15b6c8aebb9fdbaeb540aa
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2022-04-04 09:04:25 +0300

    Added 1 PY sub

---
+++ PY_Diamond_Maksim/Main_PY.cs
@@ -0,0 +1,26 @@
+﻿namespace PY_Diamond_Maksim
+{
+    class Main_PY
+    {
+        private static readonly string Path = @"C:\!Work\PY\PY_20220301_03";
+        private static readonly string SubCode = "1";
+        private static readonly bool SendToProd = false;   // true - send to Prod ; false - send to Stag
+
+        static void Main(string[] args)
+        {
+            Methods methods = new();
+
+            List<Diamond.Core.Models.LegalStatusEvent> patents = SubCode switch
+            {
+                "1" => methods.Start(Path, SubCode),
+                _ => null
+            };
+
+            Console.WriteLine();
+
+            if (patents != null) methods.SendToDiamond(patents, SendToProd);
+            else Console.WriteLine("Wrong subcode");
+        }
+    }
+}
+
