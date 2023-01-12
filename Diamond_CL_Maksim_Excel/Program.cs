b43eb78f5f6c76ded119d601b80d432d4d8454c0
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2023-01-12 10:07:03 +0100

    LENS-415. Corrected code for CL

--- Diamond_CL_Maksim_Excel/Program.cs
+++ Diamond_CL_Maksim_Excel/Program.cs
@@ -2,18 +2,18 @@
 {
     internal class Program
     {
-        private const string Path = @"D:\LENS\CL\CL_20211231_01";
+        private const string Path = @"D:\LENS\CL\CL_20221231_01";
         private const bool SendToProd = false; // true - send to Prod ; false - send to Stag
         private static void Main()
         {
             Methods methods = new();
 
-            List<Diamond.Core.Models.LegalStatusEvent> patents = methods.Start(Path);
+            var patents = methods.Start(Path);
 
             Console.WriteLine();
 
-            if (patents != null) methods.SendToDiamond(patents, SendToProd);
-            else Console.WriteLine("Wrong subcode");
+            if (patents.Count != 0) methods.SendToDiamond(patents, SendToProd);
+            else Console.WriteLine("Empty list");
         }
     }
 }
