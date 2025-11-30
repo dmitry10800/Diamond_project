aee061fc193f31aa21c192ab2ce2d9075039cabf
Maksim Shumikhin <mshumikhin@lighthouseip.com>
2025-11-30 17:06:00 +0100

    added small fixies

---
+++ Diamond_SA_Maksim/Methods.cs
@@ -0,0 +1,45 @@
+﻿using Diamond.Core.Models;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace Diamond_SA_Maksim;
+
+public class Methods
+{
+    private string _currentFileName;
+    private int _id = 1;
+
+    internal List<LegalStatusEvent> Start(string path, string subCode)
+    {
+        var statusEvents = new List<LegalStatusEvent>();
+        var directory = new DirectoryInfo(path);
+        var files = directory.GetFiles("*.tetml", SearchOption.AllDirectories).Select(file => file.FullName).ToList();
+
+        XElement tet;
+        var xElements = new List<XElement>();
+
+        foreach (var tetml in files)
+        {
+            _currentFileName = tetml;
+            tet = XElement.Load(tetml);
+
+            if (subCode == "1")
+            {
+                xElements = tet.Descendants().Where(val => val.Name.LocalName == "Text")
+                    .SkipWhile(val => !val.Value.StartsWith(""))
+                    .TakeWhile(val => !val.Value.StartsWith(""))
+                    .ToList();
+
+                //var text = MakeText(xElements, subCode);
+
+                //var notes = Regex.Split(text, @"(?=\(11\).+)", RegexOptions.Singleline).Where(val => !string.IsNullOrEmpty(val) && val.StartsWith("(11)")).ToList();
+
+                //foreach (var note in notes)
+                //{
+                //    statusEvents.Add();
+                //}
+            }
+        }
+        return statusEvents;
+    }
+}
---
+++ Diamond_SA_Maksim/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Diamond_SA_Maksim
+{
+    internal class Program
+    {
+        private const string Path = @"D:\аа\CY_20240920_4616B";
+        private const string SubCode = "1";
+        private const bool SendToProd = false; //true - send to prod; false - send to stag
+
+        private static void Main(string[] args)
+        {
+            var methods = new Methods();
+
+            var convertedPatents = SubCode switch
+            {
+                "1" => methods.Start(Path, SubCode),
+                _ => null
+            };
+
+            Console.WriteLine();
+
+            DiamondUtilities.DiamondSender.SendToDiamond(convertedPatents, SendToProd);
+        }
+    }
+}
